7241229c913ac16a8030010e18dc956f4e523f9a
DESKTOP-4H6VNN9\THANH NGUYEN <thanh>
2019-01-02 16:04:26 +0700

    croll

--- ltp/content/view.php
+++ ltp/content/view.php
@@ -7,6 +7,6 @@ if(!isset($tap)){
 }
 ?>
 
-<video class="afterglow video" id="myvideo" width="1280" height="720" poster="https://1.bp.blogspot.com/-wcd0TTzi44k/W2bbp5y5_iI/AAAAAAAA5kk/r3dcoVS7v0EA-EqjvHVgk_DUpRL4Wnt0ACLcBGAs/s1600/38289228_2197248486970753_4030785322206887936_o.jpg">
+<video class="afterglow video" autoplay id="myvideo" width="1280" height="720" poster="https://1.bp.blogspot.com/-wcd0TTzi44k/W2bbp5y5_iI/AAAAAAAA5kk/r3dcoVS7v0EA-EqjvHVgk_DUpRL4Wnt0ACLcBGAs/s1600/38289228_2197248486970753_4030785322206887936_o.jpg">
       <source type="video/mp4" src="<?php echo $arr[$tap-1];?>" />
 </video>
