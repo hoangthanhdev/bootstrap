7156bcfd324d18bad369358e915a2ea3ed94d0de
DESKTOP-4H6VNN9\THANH NGUYEN <thanh>
2019-01-08 16:22:00 +0700

    add hd

--- ltp/content/view.php
+++ ltp/content/view.php
@@ -7,7 +7,8 @@ if(!isset($tap)){
 }
 ?>
 
-<video class="afterglow video" id="myvideo" width="1280" height="720" poster="https://1.bp.blogspot.com/-wcd0TTzi44k/W2bbp5y5_iI/AAAAAAAA5kk/r3dcoVS7v0EA-EqjvHVgk_DUpRL4Wnt0ACLcBGAs/s1600/38289228_2197248486970753_4030785322206887936_o.jpg">
+<video autoplay class="afterglow video" id="myvideo" width="1280" height="720" poster="https://1.bp.blogspot.com/-wcd0TTzi44k/W2bbp5y5_iI/AAAAAAAA5kk/r3dcoVS7v0EA-EqjvHVgk_DUpRL4Wnt0ACLcBGAs/s1600/38289228_2197248486970753_4030785322206887936_o.jpg" data-skin="dark">
       <source type="video/mp4" src="<?php echo $arr[$tap-1];?>" />
+      <source type="video/mp4" src="<?php echo $arr[$tap+1];?>" data-quality="hd" /> <!-- phai cung duoi file -->
 </video>
 <?php echo $arr[$tap-1];?>
