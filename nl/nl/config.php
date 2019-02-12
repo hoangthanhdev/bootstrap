0f4d5ff406cb24d2e2f04da40b7a7acd1c91d7ac
DESKTOP-4H6VNN9\THANH NGUYEN <thanh>
2019-02-12 15:38:18 +0700

    nl

---
+++ nl/nl/config.php
@@ -0,0 +1,11 @@
+<?php	
+	header("Content-Type: text/html; charset=utf-8");
+	define('NGANLUONG_URL', 'https://sandbox.nganluong.vn:8088/nl35/checkout.php');
+	define('RECEIVER','hoangthanh0809@gmail.com'); // Email tài khoản Ngân Lượng
+	define('MERCHANT_ID', '47194'); // Mã kết nối
+	define('MERCHANT_PASS', '3f5bc14a3c32c3eb253e8db608f780f5'); // Mật khẩu kết nối 
+	
+?>
+
+
+	
