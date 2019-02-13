6c3872a7752aaf8095694948a82b3e0edb6b9166
thanh nguyen <hoangthanh.dev@gmail.com>
2019-02-13 20:34:13 +0700

    nganluong

---
+++ nganluong/config.php
@@ -0,0 +1,11 @@
+<?php	
+	header("Content-Type: text/html; charset=utf-8");
+	define('NGANLUONG_URL', 'https://sandbox.nganluong.vn:8088/nl35/checkout.php');
+	define('RECEIVER','hoangthanh0809@gmail.com'); // Email tài khoản Ngân Lượng
+	define('MERCHANT_ID', '36680'); // Mã kết nối
+	define('MERCHANT_PASS', 'bbdn123456'); // Mật khẩu kết nối 
+	
+?>
+
+
+	
